315f914193753850f8fb5e30e7106ad81462ad2b
gwy-maiso <gwynmaiso@gamil.com>
2025-10-06 18:25:57 +0800

    Week3: Assignment_3 Toggle (updated)

--- announcements/views.py
+++ announcements/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Fake "database"
 announcements = [
-    {'id': 1, 'title': 'Welcome to the Site', 'content': 'This is the first announcement. We are glad to have you here! mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm'},
-    {'id': 2, 'title': 'Exam Schedule', 'content': 'The midterm exam will start next week. Please review your notes and prepare accordingly. mdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'},
-    {'id': 3, 'title': 'Holiday Notice', 'content': 'There will be no classes on Friday due to a public holiday.'},
+    {'id': 1, 'title': 'Welcome to the Site', 'content': 'This is the first announcement. We are glad to have you here!'},
+    {'id': 2, 'title': 'Exam Schedule', 'content': 'The midterm exam will start next week. Please review your notes and prepare accordingly.'},
+    {'id': 3, 'title': 'IMPORTANT ADVISORY', 'content': 'As per the evaluation and assessment of the medical team, and to ensure everyone’s health and safety following reported cases of Hand, Foot, and Mouth Disease (HFMD), the EVSU-Main Campus will shift to online/asynchronous classes from October 6 to 10, 2025. Students are strictly advised not to enter the campus, while TechnoMart will be temporarily closed during this period. Non-teaching personnel are also directed to conduct thorough sanitation in their respective work areas. Let’s all do our part to maintain a safe and healthy campus. Please monitor your health and report any flu-like symptoms immediately. Stay safe, EVSUnistas!'},
 ]
 
 def home(request):
